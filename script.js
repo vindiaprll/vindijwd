ccc181e0cde0326bc4359b3a126ffbc7ac51d9b7
vindiaprll <vindimunik@gmail.com>
2023-12-23 18:10:50 +0700

    menambhakan file js dan mengubah file index

---
+++ script.js
@@ -0,0 +1,7 @@
+// alert('Mohon Konfirmasi sebelm mengakses website ini');
+
+// var nama = prompt('Masukkan nama');
+// alert(nama);
+
+var text = confirm('Apkah anda yakin untuk mengakses web ini?');
+alert(text);
